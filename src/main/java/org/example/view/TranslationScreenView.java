816602c36fc983c4a411b4b15e01aa08fa3381bb
kerem <mahmut@hotmail.com>
2021-06-20 17:52:26 +0300

    tranlationscreenview

---
+++ src/main/java/org/example/view/TranslationScreenView.java
@@ -0,0 +1,41 @@
+package org.example.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import org.example.App;
+import org.example.utils.Utils;
+import org.example.viewmodel.UserModelView;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TranslationScreenView {
+
+    @FXML
+    private TextArea sourceTextArea;
+    @FXML
+    private TextArea translatedTextArea;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    public void onSubmitButtonClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    public void onSaveButtonClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    public void onBackButtonClicked(ActionEvent event){
+
+    }
+
+}
